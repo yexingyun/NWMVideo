b8ad46a0a8ffb0ced97a355629c09cca07df7eff
ziyun <ziyun@yahoo.com>
2024-01-08 14:34:32 -0800

    add serve 文件

---
+++ serve.py
@@ -0,0 +1,8 @@
+from tornado.httpserver import HTTPServer
+from tornado.wsgi import WSGIContainer
+from flask_app import app
+from tornado.ioloop import IOLoop
+
+s = HTTPServer(WSGIContainer(app))
+s.listen(8088) # 监听 8080 端口
+IOLoop.current().start()
